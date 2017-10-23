91ddc7d5bf618b1353b17633044b6589a5da09aa
selva <selva@etekchnoservices.com>
2017-10-23 15:02:47 +0530

    links the layouts files

--- app/src/main/java/com/example/user/mahindra/MainActivity.java
+++ app/src/main/java/com/example/user/mahindra/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.user.mahindra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.*;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this,ServiceDashBoard.class);
+                       startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user/mahindra/ServiceDashBoard.java
+++ app/src/main/java/com/example/user/mahindra/ServiceDashBoard.java
@@ -1,8 +1,10 @@
 package com.example.user.mahindra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.*;
+import android.widget.Button;
 
 public class ServiceDashBoard extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class ServiceDashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.service_dashboard);
+        Button newservices = (Button) findViewById(R.id.newservice);
+        newservices.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(ServiceDashBoard.this,NewService.class);
+                startActivity(i);
+            }
+        });
     }
 }
 
