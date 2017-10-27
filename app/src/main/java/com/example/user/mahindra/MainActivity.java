d8f8b2fdb44f7656027d625f99923b958a0f190b
hariharaprabuU <hariharaprabu.u@gmail.com>
2017-10-27 18:22:41 +0530

    Changes for Login and toolbar

--- app/src/main/java/com/example/user/mahindra/MainActivity.java
+++ app/src/main/java/com/example/user/mahindra/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.mahindra;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -21,16 +22,19 @@ import com.microsoft.windowsazure.mobileservices.*;
 import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
 import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+import com.microsoft.windowsazure.mobileservices.table.serialization.JsonEntityParser;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-
+import java.lang.*;
 import static com.example.user.mahindra.R.id.clientPassword;
 import static com.example.user.mahindra.R.id.clientUsername;
 import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.val;
+import static java.util.logging.Level.parse;
 
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -39,21 +43,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private EditText clientUsername;
     private EditText clientPassword;
     private EditText clientUsertype;
-    //private MobileServiceTable mTable;
-
-//    ToDoItem item = new ToDoItem();
-//    item.Text = "Awesome item";
-//        mClient.getTable(ToDoItem.class).insert(item, new TableOperationCallback<item>() {
-//        public void onCompleted(ToDoItem entity, Exception exception, ServiceFilterResponse response) {
-//            if (exception == null) {
-//                // Insert succeeded
-//                System.out.println('Insert success');
-//            } else {
-//                // Insert failed
-//                System.out.println("Insert failed");
-//            }
-//        }
-//    })
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,20 +76,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 return client;
             }
         });
-//        MobileServiceTable<UsersAuth> mTable = mClient.getTable("user_auth", UsersAuth.class);
-//        try {
-//            final List<UsersAuth> result =  mTable.where().field("username").eq("admin").execute().get();
-//            for (UsersAuth item : result) {
-//                System.out.println(result);
-//            }
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        }
-
-        //System.out.println(item);
-        //mClient.getTable(ToDoItem.class).insert(item.get())
     }
 
 
@@ -141,99 +116,104 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String sSelected=parent.getItemAtPosition(position).toString();
-//        final Spinner clientUsertype =(Spinner) findViewById(R.id.clientUsertype);
-//        clientUsername = (EditText) findViewById(R.id.clientUsername);
-//        clientPassword = (EditText) findViewById(R.id.clientPassword);
+        final Spinner clientUsertype =(Spinner) findViewById(R.id.clientUsertype);
+        clientUsername = (EditText) findViewById(R.id.clientUsername);
+        clientPassword = (EditText) findViewById(R.id.clientPassword);
         //clientUsertype = (EditText) findViewById(R.id.clientUsertype);
 
         Button login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener(){
+            int ONE_TIME = 0;
             @Override
-            public void onClick(View view)
-            {
-//                System.out.println(clientUsername.getText().toString());
-//                System.out.println(clientPassword.getText().toString());
-//                System.out.println(clientUsertype.getSelectedItem().toString());
-//                final UsersAuth user = new UsersAuth();
-//                //user.username = clientUsername.getText().toString();
-//                user.username = "admin";
-//                user.password = clientPassword.getText().toString();
-//                //user.usertype = clientUsertype.getSelectedItem().toString();
-//                user.usertype = "Service Agent";
-//                final MobileServiceTable<UsersAuth> mTable = mClient.getTable("user_auth", UsersAuth.class);
-//
-//                AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
-//                    @Override
-//                    protected Void doInBackground(Void... params) {
-//                        try {
-//                            final List<UsersAuth> result = getDataFromDB();
-//                            runOnUiThread(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    for (UsersAuth item : result) {
-//                                        System.out.println(result);
-//                                    }
-//                                }
-//                            });
-//                        } catch (final Exception e) {
-//                            createAndShowDialogFromTask(e, "Error");
-//                        }
-//
-//                        return null;
-//                    }
-//                };
-//
-//                runAsyncTask(task);
+            public void onClick(View view) {
+                ONE_TIME++;
+                if (ONE_TIME == 1) {
+                    System.out.println(clientUsername.getText().toString());
+                    System.out.println(clientPassword.getText().toString());
+                    System.out.println(clientUsertype.getSelectedItem().toString());
+                    final UsersAuth user = new UsersAuth();
+                    user.username = clientUsername.getText().toString();
+                    //user.username = "admin";
+                    user.password = clientPassword.getText().toString();
+                    //user.password = "password";
+                    user.usertype = clientUsertype.getSelectedItem().toString();
+                    //user.usertype = "Quality Agent";
+                    final MobileServiceTable<UsersAuth> mTable = mClient.getTable("user_auth", UsersAuth.class);
+
+                    AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Void... params) {
+                            try {
+                                final List<UsersAuth> result = mTable.where().field("username").eq(val(user.username)).and().field("usertype").eq(val(user.usertype)).select("user_password").execute().get();
+                                StringBuilder commaSepValueBuilder = new StringBuilder();
+
+                                //Looping through the list
+                                for (int i = 0; i < result.size(); i++) {
+                                    commaSepValueBuilder.append(result.get(i));
+
+                                    if (i != result.size() - 1) {
+                                        commaSepValueBuilder.append(", ");
+                                    }
+                                }
+                                final String Password = commaSepValueBuilder.toString();
+                                System.out.println(Password);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        ONE_TIME = 0;
+                                        if(Password.equals("")){
+                                            createAndShowDialog("Please check your username", "Wrong");
+                                        }
+                                        else if (user.password.equals(Password)) {
+                                            System.out.println("Success Login");
+                                            Toast.makeText(MainActivity.this, "Logging in!", Toast.LENGTH_SHORT).show();
+//                                            SharedPreferences prefs = getSharedPreferences("Username", MODE_PRIVATE);
+//                                            prefs.edit().putString("username", user.username).commit();
+                                            Intent intent = new Intent(MainActivity.this, ServiceDashBoard.class);
+                                            intent.putExtra("username", user.username);
+                                            startActivity(intent);
+                                        }
+
+                                        else {
+                                            createAndShowDialog("Your password is wrong! Please check it and try again!!!", "Wrong");
+                                            System.out.println("Failed");
+                                            //showDialogBox();
+                                        }
+                                    }
+                                });
 
 
-                //Intent i = new Intent(MainActivity.this,ServiceDashBoard.class);
-                //startActivity(i);
-                MobileServiceTable<vehicle> mTable = mClient.getTable("vehicle", vehicle.class);
-                final vehicle item = new vehicle();
-                item.reg_no = "PY 01 C 1234";
-                item.eng_no = "BSRT4568N";
-                item.col_code = "Grey";
-////        mClient.getTable(vehicle.class).insert(item, new TableOperationCallback<item>() {
-////            public void onCompleted(vehicle entity, Exception exception, ServiceFilterResponse response) {
-////                if (exception == null) {
-////                    // Insert succeeded
-////                } else {
-////                    // Insert failed
-////                }
-////            }
-////        });
-                try {
-                    vehicle entity = mClient.getTable(vehicle.class).insert(item).get();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
+                            } catch (final Exception e) {
+                                createAndShowDialogFromTask(e, "Error");
+                            }
+
+                            return null;
+                        }
+                    };
+
+                    runAsyncTask(task);
                 }
-            }
+             }
+
         });
 
     }
 
-
+    public void showDialogBox(){
+        createAndShowDialog("Your password is wrong! Please check it and try again!!!","Wrong");
+    }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
 
-//    private List<UsersAuth> getDataFromDB() throws ExecutionException, InterruptedException
-//    {
-//        String table = "complaint_id";
-//        String user = "admin";
-//        final MobileServiceTable<UsersAuth> mTable = mClient.getTable("complaint", UsersAuth.class);
-//        return mTable.where().field(table).eq(1).execute().get();
-//    }
-//    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-//            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-//        } else {
-//            return task.execute();
-//        }
-//    }
+    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else {
+            return task.execute();
+        }
+    }
 
 
 }
